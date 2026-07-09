ad9526de810622a64049a01fb3d22f9eb6fafd6f
VictorFranklinM <victorfranklin2006@gmail.com>
2026-07-09 14:09:09 -0300

    feat: color selection for both characters

--- scripts/selection_screen.gd
+++ scripts/selection_screen.gd
@@ -21,43 +21,42 @@ var current_palette_p1 := 0
 var current_palette_p2 := 0
 
 func _ready():
-	apply_palette(player1_sprite)
-	apply_palette(player2_sprite)
+	apply_palette(player1_sprite, palettes_mag, current_palette_p1)
+	apply_palette(player2_sprite, palettes_clown, current_palette_p2)
 
-func apply_palette(player: AnimatedSprite2D):
-	player.material.set_shader_parameter("new_palette", palettes_clown[current_palette_p1])
+func apply_palette(sprite: AnimatedSprite2D, palettes: Array, current_palette: int):
+	sprite.material.set_shader_parameter("new_palette", palettes[current_palette])
 
 func _on_left_pressed(player: int) -> void:
 	if (player == 1):
 		current_palette_p1 -= 1
 		if current_palette_p1 < 0:
-			current_palette_p1 = palettes_clown.size() - 1
-		apply_palette(player1_sprite)
+			current_palette_p1 = palettes_mag.size() - 1
+		apply_palette(player1_sprite, palettes_mag, current_palette_p1)
 	elif (player == 2):
 		current_palette_p2 -= 1
 		if current_palette_p2 < 0:
 			current_palette_p2 = palettes_clown.size() - 1
-		apply_palette(player2_sprite)
+		apply_palette(player2_sprite, palettes_clown, current_palette_p2)
 
 
 func _on_right_pressed(player: int) -> void:
 	if (player == 1):
 		current_palette_p1 += 1
-		if current_palette_p1 >= palettes_clown.size():
+		if current_palette_p1 >= palettes_mag.size():
 			current_palette_p1 = 0
-		apply_palette(player1_sprite)
+		apply_palette(player1_sprite, palettes_mag, current_palette_p1)
 	elif (player == 2):
 		current_palette_p2 += 1
 		if current_palette_p2 >= palettes_clown.size():
 			current_palette_p2 = 0
-		apply_palette(player2_sprite)
+		apply_palette(player2_sprite, palettes_clown, current_palette_p2)
 
-
-func _on_left_p1_button_down() -> void:
+func _on_left_p_1_pressed() -> void:
 	_on_left_pressed(1)
-func _on_right_p1_button_down() -> void:
+func _on_right_p_1_pressed() -> void:
 	_on_right_pressed(1)
-func _on_left_p2_button_down() -> void:
+func _on_left_p_2_pressed() -> void:
 	_on_left_pressed(2)
-func _on_right_p2_button_down() -> void:
+func _on_right_p_2_pressed() -> void:
 	_on_right_pressed(2)
